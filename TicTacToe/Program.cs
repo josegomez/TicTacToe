8c61909ed02d8b80073f5a5a29de8b714abce523
BVAnderson <46540395+BVAnderson@users.noreply.github.com>
2020-04-15 22:36:47 -0500

    Update Program.cs

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -14,17 +14,42 @@ namespace TicTacToe
         {
             //Declares a 3x3 array for the board. 
             //Any given position in the board can be accessed by the x,y coordiantes IE: tickTacToeBoard[1,2]
-            string[,] tickTacToeBoard= new string[3,3];
+            string[,] tickTacToeBoard = new string[3, 3];
 
             ResetBoard(tickTacToeBoard);
-            PrintBoard(tickTacToeBoard);
-            
+
+
             //Game Loop
-            while(!CheckWinner(tickTacToeBoard))
+            while (!CheckWinner(tickTacToeBoard))
             {
+                Console.Clear();
+                PrintBoard(tickTacToeBoard);
                 Console.Write($"Player {currentPlayer} Enter Position to Play (1-9):");
-                string enteredPosition = Console.ReadLine(); // Check the user entered 1-9 and if that space is avaialble then set it to X or Y then check if someone won...
-                //Print the board after every turn and swap between X and Y player alternating
+                string enteredPosition = Console.ReadLine();// Check the user entered 1-9 and if that space is avaialble then set it to X or Y then check if someone won...
+                int enteredNum;
+                bool canConvert = int.TryParse(enteredPosition, out enteredNum);
+                if (canConvert == true)
+                {
+                    if (enteredNum > 0 && enteredNum < 10)
+                    {
+                        //Print the board after every turn and swap between X and Y player alternating
+                        int y = (enteredNum - 1) / 3;
+                        int x = (enteredNum - (y * 3)) - 1;
+                        if (tickTacToeBoard[y, x] != "X" && tickTacToeBoard[y, x] != "O")
+                        {
+                            tickTacToeBoard[y, x] = currentPlayer;
+                            if (currentPlayer == "X")
+                            {
+                                currentPlayer = "O";
+                            }
+                            else if (currentPlayer == "O")
+                            {
+                                currentPlayer = "X";
+                            }
+                            //PrintBoard(tickTacToeBoard);
+                        }
+                    }
+                }
             }
 
         }
@@ -32,8 +57,56 @@ namespace TicTacToe
         private static bool CheckWinner(string[,] tickTacToeBoard)
         {
             //TODO: Implement Logic check to see if someone won.
+            for (int i = 0; i < 3; i++)
+            {
+                string[] checkarray = new string[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    checkarray[j] = tickTacToeBoard[i, j];
+                }
+                if (CheckMyArray(checkarray) == true)
+                {
+                    Console.WriteLine($"{checkarray[0]} is the winner!");
+                    Console.ReadLine();
+                    return true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                string[] checkarray = new string[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    checkarray[j] = tickTacToeBoard[j, i];
+                }
+                if (CheckMyArray(checkarray) == true)
+                {
+                    Console.WriteLine($"{checkarray[0]} is the winner!");
+                    Console.ReadLine();
+                    return true;
+                }
+            }
+            string[] crossCheckarray = new string[3];
+            crossCheckarray[0] = tickTacToeBoard[0, 0];
+            crossCheckarray[1] = tickTacToeBoard[1, 1];
+            crossCheckarray[2] = tickTacToeBoard[2, 2];
+            if (CheckMyArray(crossCheckarray) == true)
+            {
+                Console.WriteLine($"{crossCheckarray[0]} is the winner!");
+                Console.ReadLine();
+                return true;
+            }
+            crossCheckarray[0] = tickTacToeBoard[0, 2];
+            crossCheckarray[1] = tickTacToeBoard[1, 1];
+            crossCheckarray[2] = tickTacToeBoard[2, 0];
+            if (CheckMyArray(crossCheckarray) == true)
+            {
+                Console.WriteLine($"{crossCheckarray[0]} is the winner!");
+                Console.ReadLine();
+                return true;
+            }
             return false;
         }
+
 
         /// <summary>
         /// Resets the board up, placing a sequential number on each box to allow the players to play
@@ -42,15 +115,27 @@ namespace TicTacToe
         private static void ResetBoard(string[,] tickTacToeBoard)
         {
             int ct = 1;
-            for (int i = 0; i < 3; i ++)
+            for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    tickTacToeBoard[i, j] = (ct++).ToString();   
+                    tickTacToeBoard[i, j] = (ct++).ToString();
                 }
             }
         }
 
+        public static bool CheckMyArray(string[] myCheckArray)
+        {
+            if (myCheckArray[0] == myCheckArray[1])
+            {
+                if (myCheckArray[1] == myCheckArray[2])
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
         /// <summary>
         /// Prints the Game Board
         /// </summary>
